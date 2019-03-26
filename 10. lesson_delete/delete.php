60a162cbdd183c2eaf5bdc5bd6291e86445a02b3
Aleksandr Kononov <mister.San4es@list.ru>
2019-03-26 23:42:24 +0400

    10.delete

---
+++ 10. lesson_delete/delete.php
@@ -0,0 +1,23 @@
+<?php
+//получение id записи
+$id = $_GET['id'];
+
+// Подготовка и выполнение запроса к базе данных
+$pdo = new PDO('mysql:host=localhost; dbname=task-manager', 'root', ''); // подключение к БД
+$sql = 'SELECT * FROM tasks WHERE id=:id'; // запрос
+$statement = $pdo->prepare($sql);  //подготовка и
+$statement->execute([':id' => $id]); // выполнение запроса к БД
+$task = $statement->fetch(PDO::FETCH_ASSOC);
+
+// удаляем текущую картинку если существует
+if(file_exists('uploads/' . $task['image'])) {
+    unlink('uploads/' . $task[$sql]);
+}
+
+// подготока и выполнение запроса к БД
+$sql = 'DELETE FROM tasks WHERE id=:id';
+$statement = $pdo->prepare($sql);
+$statement->execute(([':id' => $id]));
+
+header('Location: index.php');
+exit;
